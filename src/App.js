9ffda6d21d176dbeb8fa420a9eac27bf3c0942bd
zuev720 <zuev720mail.ru>
2021-07-18 14:31:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import ShopItemFunc from './components/ShopItemFunc';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <div className="background-element">
+        </div>
+        <div className="highlight-window">
+          <div className='highlight-overlay'></div>
+        </div>
+        <div className="window">
+          <ShopItemFunc />
+        </div>
+      </div>
   );
 }
 
---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,27 @@
+export default function ShopItemFunc() {
+    const obj = {
+        brand: 'Tiger of Sweden',
+        title: 'Leonard coat',
+        description: 'Minimalistic coat in cotton-blend',
+        descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+        price: 399,
+        currency: '£'
+    };
+
+    return (
+        <div className="main-content">
+            <h2>{obj.brand}</h2>
+            <h1>{obj.title }</h1>
+            <h3>{obj.description}</h3>
+            <div className="description">{ obj.descriptionFull }</div>
+            <div className="highlight-window mobile">
+                <div className="highlight-overlay"></div>
+            </div>
+            <div className="divider"></div>
+            <div className="purchase-info">
+                <div className="price">{obj.currency + obj.price + '.00' }</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+    );
+}
